eca4038537025b31675af27f614b096af3e035db
David Braley <david.braley@gmail.com>
2018-07-03 12:40:58 -0400

    Another first basic test

--- src/test/java/FizzBUzz/FizzBuzzTest.java
+++ src/test/java/FizzBUzz/FizzBuzzTest.java
@@ -5,10 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
-    @Test
-    public void fizzBuzzBasicTest() {
-        FizzBuzz tester = new FizzBuzz();
 
-        assertEquals(100, tester.printStuff(100), "print given number as the same");
+    @Test
+    public void fizzBuzzZestyTest() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        assertEquals("1 2", fizzBuzz.printStuff(2));
     }
 }
