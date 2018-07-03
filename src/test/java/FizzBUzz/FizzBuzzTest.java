19d001c518c6689412c166a37c43ed43b74dd0e9
David Braley <david.braley@gmail.com>
2018-07-03 12:44:53 -0400

    Add test for upto 3

--- src/test/java/FizzBUzz/FizzBuzzTest.java
+++ src/test/java/FizzBUzz/FizzBuzzTest.java
@@ -7,9 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzTest {
 
     @Test
-    public void fizzBuzzZestyTest() {
+    public void fizzBuzz_testForUpto2() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-
         assertEquals("1 2", fizzBuzz.printStuff(2));
+    }
+
+    @Test
+    void fizzBuzz_testForUpto3() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("1 2 Fizz", fizzBuzz.printStuff(3));
     }
 }
