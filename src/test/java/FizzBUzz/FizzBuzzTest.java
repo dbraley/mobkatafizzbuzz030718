06b0b9607aa5c5d4554b9f17b725e65589f52ef3
David Braley <david.braley@gmail.com>
2018-07-03 13:02:00 -0400

    Now works upto 5

--- src/test/java/FizzBUzz/FizzBuzzTest.java
+++ src/test/java/FizzBUzz/FizzBuzzTest.java
@@ -1,20 +1,30 @@
 package FizzBUzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    public void setup() {
+        fizzBuzz = new FizzBuzz();
+    }
 
     @Test
     public void fizzBuzz_testForUpto2() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("1 2", fizzBuzz.printStuff(2));
     }
 
     @Test
     void fizzBuzz_testForUpto3() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("1 2 Fizz", fizzBuzz.printStuff(3));
+    }
+
+    @Test
+    void fizzBuzz_testForUpto5() {
+        assertEquals("1 2 Fizz 4 Buzz", fizzBuzz.printStuff(5));
     }
 }
